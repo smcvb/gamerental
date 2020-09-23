372d24642b28f24f5271223464210636aa7473eb
Steven van Beelen <steven.vanbeelen@axoniq.io>
2020-09-23 16:16:56 +0200

    Initial commit for gamerental application
    Initial commit for the game-rental application,
     to show case Axon Framework features on various occassions.

---
+++ src/main/java/io/axoniq/demo/gamerental/GamerentalApplication.java
@@ -0,0 +1,13 @@
+package io.axoniq.demo.gamerental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamerentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamerentalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/axoniq/demo/gamerental/GamerentalApplicationTests.java
@@ -0,0 +1,13 @@
+package io.axoniq.demo.gamerental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamerentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
