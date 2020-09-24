f2e5f6a1ddf7e3b245aa5fc6bf33b7639a085f23
Steven van Beelen <steven.vanbeelen@axoniq.io>
2020-09-24 16:02:50 +0200

    Further personalisation
    - Adjust style of auto created classes
    - Spacing and clarification in pom.xml

--- src/main/java/io/axoniq/demo/gamerental/GamerentalApplication.java
+++ src/main/java/io/axoniq/demo/gamerental/GamerentalApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GamerentalApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GamerentalApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(GamerentalApplication.class, args);
+    }
 }
--- src/test/java/io/axoniq/demo/gamerental/GamerentalApplicationTests.java
+++ src/test/java/io/axoniq/demo/gamerental/GamerentalApplicationTests.java
@@ -1,13 +1,12 @@
 package io.axoniq.demo.gamerental;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class GamerentalApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
