3ceef30a8f4ceda8021a48b3f60b8a58c2a6e886
Steven van Beelen <steven.vanbeelen@axoniq.io>
2020-12-07 14:06:48 +0100

    Add message dispatching/handling logging
    Add a LoggingInterceptor to the command-, query- and event bus, as well
    as the event processors. This way we can see all messages flowing in the
     terminal

--- src/main/java/io/axoniq/demo/gamerental/ApplicationConfig.java
+++ src/main/java/io/axoniq/demo/gamerental/ApplicationConfig.java
@@ -3,11 +3,18 @@ package io.axoniq.demo.gamerental;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.axonframework.commandhandling.CommandBus;
 import org.axonframework.commandhandling.distributed.AnnotationRoutingStrategy;
 import org.axonframework.commandhandling.distributed.RoutingStrategy;
 import org.axonframework.commandhandling.distributed.UnresolvedRoutingKeyPolicy;
+import org.axonframework.config.EventProcessingConfigurer;
+import org.axonframework.eventhandling.EventBus;
+import org.axonframework.messaging.Message;
+import org.axonframework.messaging.interceptors.LoggingInterceptor;
+import org.axonframework.queryhandling.QueryBus;
 import org.axonframework.serialization.Serializer;
 import org.axonframework.serialization.json.JacksonSerializer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -31,5 +38,36 @@ public class ApplicationConfig {
     @Bean
     public RoutingStrategy routingStrategy() {
         return new AnnotationRoutingStrategy(UnresolvedRoutingKeyPolicy.RANDOM_KEY);
+    }
+
+    @Bean
+    public LoggingInterceptor<Message<?>> loggingInterceptor() {
+        return new LoggingInterceptor<>();
+    }
+
+    @Autowired
+    public void configureLoggingInterceptorFor(CommandBus commandBus,
+                                               LoggingInterceptor<Message<?>> loggingInterceptor) {
+        commandBus.registerDispatchInterceptor(loggingInterceptor);
+        commandBus.registerHandlerInterceptor(loggingInterceptor);
+    }
+
+    @Autowired
+    @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
+    public void configureLoggingInterceptorFor(EventBus eventBus, LoggingInterceptor<Message<?>> loggingInterceptor) {
+        eventBus.registerDispatchInterceptor(loggingInterceptor);
+    }
+
+    @Autowired
+    public void configureLoggingInterceptorFor(EventProcessingConfigurer eventProcessingConfigurer,
+                                               LoggingInterceptor<Message<?>> loggingInterceptor) {
+        eventProcessingConfigurer.registerDefaultHandlerInterceptor((config, processorName) -> loggingInterceptor);
+    }
+
+    @Autowired
+    @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
+    public void configureLoggingInterceptorFor(QueryBus queryBus, LoggingInterceptor<Message<?>> loggingInterceptor) {
+        queryBus.registerDispatchInterceptor(loggingInterceptor);
+        queryBus.registerHandlerInterceptor(loggingInterceptor);
     }
 }
