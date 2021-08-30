0e8f1a0941d70540fe9dfff8f25c06d23bfb0fe5
Steven van Beelen <steven.vanbeelen@axoniq.io>
2021-08-30 15:02:46 +0200

    Further prepare the application for step 9
    - add a run config for RSocket
    - add a run config to run everything combined
    - add onErrorDropped to solve ugly warning from RSocket 1.1.1
    - update the axonserver properties
    - add rsocket dedicated properties to specify a port for RSocket
    - add test properties including profile rsocket and disabling axon
    server
    - update spring boot starter parent version
    - add rsocket spring boot starter dependency
    
    #step8

--- src/main/java/io/axoniq/demo/gamerental/ApplicationConfig.java
+++ src/main/java/io/axoniq/demo/gamerental/ApplicationConfig.java
@@ -16,15 +16,22 @@ import org.axonframework.messaging.interceptors.LoggingInterceptor;
 import org.axonframework.queryhandling.QueryBus;
 import org.axonframework.serialization.Serializer;
 import org.axonframework.serialization.json.JacksonSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import reactor.core.publisher.Hooks;
 
+import java.lang.invoke.MethodHandles;
 import java.time.Duration;
+import java.util.function.Consumer;
 
 @Configuration
 public class ApplicationConfig {
+
+    private static final Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
     @Bean
     @Qualifier("messageSerializer")
@@ -86,5 +93,20 @@ public class ApplicationConfig {
                 (query, result) -> result.onErrorMap(ExceptionMapper::mapRemoteException)
         );
         queryGateway.registerResultHandlerInterceptor((query, result) -> result.timeout(Duration.ofMillis(500)));
+    }
+
+    /**
+     * This {@link Hooks#onErrorDropped(Consumer)} is included as a recommendation from RSocket Java's GitHub issue
+     * [#1018](https://github.com/rsocket/rsocket-java/issues/1018). Ideally the problem would be taken care off by
+     * RSocket, but at this stage the below solution is recommended by a contributor. To be certain not all exception
+     * are blocked, only the {@code "Exceptions$ErrorCallbackNotImplemented"} is covered.
+     */
+    @Autowired
+    public void configureHooks() {
+        Hooks.onErrorDropped(t -> {
+            if (!t.getClass().toString().equals("class reactor.core.Exceptions$ErrorCallbackNotImplemented")) {
+                logger.warn("Invoked onErrorDropped for exception [{}]", t.getClass(), t);
+            }
+        });
     }
 }
