7ba7c161b3854e69b6d41a4a38bcf929d4980bff
Steven van Beelen <steven.vanbeelen@axoniq.io>
2022-03-28 15:32:05 +0200

    Circular dependency issue resolution
    Change the LoggingInterceptor configuration approach to resolve the
    circular dependency.

--- src/main/java/io/axoniq/demo/gamerental/ApplicationConfig.java
+++ src/main/java/io/axoniq/demo/gamerental/ApplicationConfig.java
@@ -4,14 +4,14 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.axonframework.commandhandling.CommandBus;
-import org.axonframework.config.EventProcessingConfigurer;
+import org.axonframework.config.ConfigurerModule;
 import org.axonframework.eventhandling.EventBus;
+import org.axonframework.lifecycle.Phase;
 import org.axonframework.messaging.Message;
 import org.axonframework.messaging.interceptors.LoggingInterceptor;
 import org.axonframework.queryhandling.QueryBus;
 import org.axonframework.serialization.Serializer;
 import org.axonframework.serialization.json.JacksonSerializer;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -37,27 +37,27 @@ public class ApplicationConfig {
         return new LoggingInterceptor<>();
     }
 
-    @Autowired
-    public void configureLoggingInterceptorFor(CommandBus commandBus,
-                                               LoggingInterceptor<Message<?>> loggingInterceptor) {
-        commandBus.registerDispatchInterceptor(loggingInterceptor);
-        commandBus.registerHandlerInterceptor(loggingInterceptor);
-    }
-
-    @Autowired
-    public void configureLoggingInterceptorFor(EventBus eventBus, LoggingInterceptor<Message<?>> loggingInterceptor) {
-        eventBus.registerDispatchInterceptor(loggingInterceptor);
-    }
-
-    @Autowired
-    public void configureLoggingInterceptorFor(EventProcessingConfigurer eventProcessingConfigurer,
-                                               LoggingInterceptor<Message<?>> loggingInterceptor) {
-        eventProcessingConfigurer.registerDefaultHandlerInterceptor((config, processorName) -> loggingInterceptor);
-    }
-
-    @Autowired
-    public void configureLoggingInterceptorFor(QueryBus queryBus, LoggingInterceptor<Message<?>> loggingInterceptor) {
-        queryBus.registerDispatchInterceptor(loggingInterceptor);
-        queryBus.registerHandlerInterceptor(loggingInterceptor);
+    /**
+     * Using a {@link ConfigurerModule} provides a means to register components as part of the start and shutdown cycles
+     * of Axon Framework. The {@link LoggingInterceptor} is registered in the latest phase on start-up for the buses.
+     * Framework issue #2061 (https://github.com/AxonFramework/AxonFramework/pull/2061) would've simplified this
+     * further, but 4.6.0 isn't finished yet.
+     */
+    @Bean
+    public ConfigurerModule loggingInterceptorConfigurerModule(LoggingInterceptor<Message<?>> loggingInterceptor) {
+        return configurer -> {
+            configurer.onInitialize(config -> config.onStart(Phase.INSTRUCTION_COMPONENTS, () -> {
+                CommandBus commandBus = config.commandBus();
+                commandBus.registerDispatchInterceptor(loggingInterceptor);
+                commandBus.registerHandlerInterceptor(loggingInterceptor);
+                EventBus eventBus = config.eventBus();
+                eventBus.registerDispatchInterceptor(loggingInterceptor);
+                QueryBus queryBus = config.queryBus();
+                queryBus.registerDispatchInterceptor(loggingInterceptor);
+                queryBus.registerHandlerInterceptor(loggingInterceptor);
+            }));
+            configurer.eventProcessing()
+                      .registerDefaultHandlerInterceptor((c, processorName) -> loggingInterceptor);
+        };
     }
 }
