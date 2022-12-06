97f6dcd165e7a1c443b1e5c2b1c39e9a186ea441
Steven van Beelen <steven.vanbeelen@axoniq.io>
2022-12-06 13:38:32 +0100

    Add the dead-letter management logic
    Add the dead-letter management logic in the form of a processor and a
    controller. The processor will retrieve the SequencedDeadLetterProcessor
     based on a processing group name.
    
    #step10

---
+++ src/main/java/io/axoniq/demo/gamerental/controller/DeadLetterProcessor.java
@@ -0,0 +1,28 @@
+package io.axoniq.demo.gamerental.controller;
+
+import org.axonframework.config.EventProcessingConfiguration;
+import org.axonframework.eventhandling.EventMessage;
+import org.axonframework.messaging.deadletter.SequencedDeadLetterProcessor;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@Profile("dead-letters")
+@Component
+class DeadLetterProcessor {
+
+    private final EventProcessingConfiguration processingConfiguration;
+    private final ExecutorService executorService;
+
+    public DeadLetterProcessor(EventProcessingConfiguration processingConfiguration) {
+        this.processingConfiguration = processingConfiguration;
+        this.executorService = Executors.newFixedThreadPool(5);
+    }
+
+    public CompletableFuture<Boolean> processorAnyFor(String processingGroup) {
+        return CompletableFuture.completedFuture(true);
+    }
+}
