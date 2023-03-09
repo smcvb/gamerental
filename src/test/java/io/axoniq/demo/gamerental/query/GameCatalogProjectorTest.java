daeff5f40f67d24c5aca2499ff7c3772fdb2fcab
Steven van Beelen <steven.vanbeelen@axoniq.io>
2023-03-09 10:26:52 +0100

    Run AF 4.7 and SpringBoot 3 migration recipes
    Run AF 4.7 and SpringBoot 3 migration recipes
    
    #step9

--- src/test/java/io/axoniq/demo/gamerental/query/GameCatalogProjectorTest.java
+++ src/test/java/io/axoniq/demo/gamerental/query/GameCatalogProjectorTest.java
@@ -12,7 +12,7 @@ import org.axonframework.queryhandling.QueryUpdateEmitter;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.ArgumentCaptor;
+import org.mockito.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
