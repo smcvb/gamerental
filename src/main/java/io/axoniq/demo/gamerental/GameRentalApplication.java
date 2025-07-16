c39d930450cfb17c49a3fdbc3bdd5547b9065f22
Steven van Beelen <steven.vanbeelen@axoniq.io>
2025-07-16 17:42:43 +0200

    Kill everything instead of ugly main to test the update checker
    Kill everything instead of ugly main to test the update checker
    
    #test/update-checker-af4

--- src/main/java/io/axoniq/demo/gamerental/GameRentalApplication.java
+++ src/main/java/io/axoniq/demo/gamerental/GameRentalApplication.java
@@ -1,12 +1,20 @@
 package io.axoniq.demo.gamerental;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.axonframework.config.Configuration;
+import org.axonframework.config.DefaultConfigurer;
+import org.axonframework.updates.UpdateChecker;
 
-@SpringBootApplication
 public class GameRentalApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(GameRentalApplication.class, args);
+    public static void main(String[] args) throws InterruptedException {
+        Configuration config = DefaultConfigurer.defaultConfiguration()
+                                                .buildConfiguration();
+
+        System.out.println("build!");
+        UpdateChecker component = config.getComponent(UpdateChecker.class);
+        config.start();
+        System.out.println("started!");
+        config.shutdown();
+        System.out.println("shutdowned!");
     }
 }
--- src/main/java/io/axoniq/demo/gamerental/query/reservations/ReservationService.java
+++
@@ -1,6 +0,0 @@
-package io.axoniq.demo.gamerental.query.reservations;
-
-interface ReservationService {
-
-    boolean notifyGameAvailability(String gameId);
-}
