18cd1a94ad4e8140660a8d5025f5d8f303b0887f
Steven van Beelen <steven.vanbeelen@axoniq.io>
2025-07-16 16:20:27 +0200

    Upgrade from M2 to M2.1
    Upgrade from M2 to M2.1
    
    #af5/milestone-playground

--- src/main/java/io/axoniq/demo/gamerental/ApplicationConfig.java
+++ src/main/java/io/axoniq/demo/gamerental/ApplicationConfig.java
@@ -2,14 +2,8 @@ package io.axoniq.demo.gamerental;
 
 import jakarta.annotation.Nonnull;
 import org.axonframework.axonserver.connector.AxonServerConfiguration;
-import org.axonframework.commandhandling.gateway.CommandGateway;
-import org.axonframework.configuration.ApplicationConfigurer;
 import org.axonframework.configuration.ComponentRegistry;
 import org.axonframework.configuration.ConfigurationEnhancer;
-import org.axonframework.eventsourcing.configuration.EventSourcedEntityModule;
-import org.axonframework.eventsourcing.configuration.EventSourcingConfigurer;
-import org.axonframework.queryhandling.QueryGateway;
-import org.axonframework.queryhandling.QueryUpdateEmitter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -33,48 +27,5 @@ public class ApplicationConfig {
                 return Integer.MIN_VALUE;
             }
         };
-    }
-
-    @Bean
-    public static ApplicationConfigurer axonConfigurer(EventSourcedEntityModule<String, ?> gameModule) {
-        return EventSourcingConfigurer.create()
-                                      .componentRegistry(cr -> cr.registerModule(gameModule))
-                                      .componentRegistry(cr -> cr.registerEnhancer(
-                                              new ConfigurationEnhancer() {
-                                                  @Override
-                                                  public void enhance(@Nonnull ComponentRegistry registry) {
-                                                      registry.registerComponent(AxonServerConfiguration.class, c -> {
-                                                          AxonServerConfiguration axonConfig = new AxonServerConfiguration();
-                                                          axonConfig.setContext("game-rental");
-                                                          return axonConfig;
-                                                      });
-                                                  }
-
-                                                  @Override
-                                                  public int order() {
-                                                      return Integer.MIN_VALUE;
-                                                  }
-                                              }
-                                      ));
-    }
-
-    @Bean
-    public org.axonframework.configuration.Configuration axonConfiguration(ApplicationConfigurer axonConfigurer) {
-        return axonConfigurer.start();
-    }
-
-    @Bean
-    public CommandGateway commandGateway(org.axonframework.configuration.Configuration axonConfiguration) {
-        return axonConfiguration.getComponent(CommandGateway.class);
-    }
-
-    @Bean
-    public QueryGateway queryGateway(org.axonframework.configuration.Configuration axonConfiguration) {
-        return axonConfiguration.getComponent(QueryGateway.class);
-    }
-
-    @Bean
-    public QueryUpdateEmitter queryUpdateEmitter(org.axonframework.configuration.Configuration axonConfiguration) {
-        return axonConfiguration.getComponent(QueryUpdateEmitter.class);
     }
 }
